355d61fcb37641f18e97d7dd08cfb9f5cc5d99bf
Haitam Moubal <haitamfox12@gmail.com>
2022-08-12 07:18:49 +0100

    fixed norm

--- includes/CUB3D.h
+++ includes/CUB3D.h
@@ -6,19 +6,19 @@
 /*   By: hmoubal <hmoubal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 17:18:31 by hmoubal           #+#    #+#             */
-/*   Updated: 2022/08/10 11:19:33 by hmoubal          ###   ########.fr       */
+/*   Updated: 2022/08/12 07:18:37 by hmoubal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
-#define CUB3D_H
+# define CUB3D_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <math.h>
-#include <fcntl.h>
-#include "mlx.h"
-#include "../libft/includes/libft.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <math.h>
+# include <fcntl.h>
+# include "mlx.h"
+# include "../libft/includes/libft.h"
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,16 +6,16 @@
 /*   By: hmoubal <hmoubal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 17:17:40 by hmoubal           #+#    #+#             */
-/*   Updated: 2022/08/10 11:21:20 by hmoubal          ###   ########.fr       */
+/*   Updated: 2022/08/12 07:18:20 by hmoubal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/CUB3D.h"
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
 	(void)ac;
 	(void)av;
-	printf("length == %zu\n",ft_strlen("ha"));
+	printf("length == %zu\n", ft_strlen("ha"));
 	return (0);
 }
