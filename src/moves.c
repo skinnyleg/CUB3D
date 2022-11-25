1fb403ddb164dcfb245e5f3b6f4e5b49eda10a79
med-doba <mac@MacBook-Pro-de-mac.local>
2022-11-25 15:19:49 +0100

    send rays from player

--- src/moves.c
+++ src/moves.c
@@ -6,7 +6,7 @@
 /*   By: med-doba <med-doba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 15:52:57 by med-doba          #+#    #+#             */
-/*   Updated: 2022/11/22 16:05:32 by med-doba         ###   ########.fr       */
+/*   Updated: 2022/11/24 13:24:44 by med-doba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ int	ft_checkwall(t_global *all, double x, double y)
 	printf("char == %c\n", all->map->map[tmp_y][tmp_x]);
 	if (all->map->map[tmp_y][tmp_x] == '1')
 		return (0);
-	// if (all->map->map[(int)tmp_y][(int)tmp_x] == '0')
+	// else if (all->map->map[tmp_y][tmp_x] == '0')
 	// 	return (1);
-	return (1;
+	return (1);
 }
 
 void	ft_move_left(t_global *all)
